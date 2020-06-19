973aa711ee870c3254cfe0403e59802208762353
Elham <Elham@Elhams-MacBook-Pro.local>
2020-06-19 21:45:45 +1000

    udate the wrangiling script

--- wrangling_scripts/wrangle_data.py
+++ wrangling_scripts/wrangle_data.py
@@ -1,8 +1,14 @@
-import pandas as pd
-import plotly.graph_objs as go
+#import the required libs first.
 
-# Use this file to read in your data and prepare the plotly visualizations. The path to the data files are in
-# `data/file_name.csv`
+import pandas as pd
+import numpy as np
+import datetime
+from datetime import date
+import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+import yfinance as yf
+from pandas_datareader import data as pdr
+%matplotlib inline
 
 def return_figures():
     """Creates four plotly visualizations
@@ -14,71 +20,179 @@ def return_figures():
         list (dict): list containing the four plotly visualizations
 
     """
-
-    # first chart plots arable land from 1990 to 2015 in top 10 economies 
-    # as a line chart
     
-    graph_one = []    
-    graph_one.append(
-      go.Scatter(
-      x = [0, 1, 2, 3, 4, 5],
-      y = [0, 2, 4, 6, 8, 10],
-      mode = 'lines'
-      )
-    )
+    #setup the holding period as 1, 2, 3 years
 
-    layout_one = dict(title = 'Chart One',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label'),
+    def get_stock_history (tiker, invest_period):
+    
+        """
+        input: 
+              tiker - stock name
+              invest_period 
+    
+        output: 
+               stock_history_adjclose
+    
+        """
+    
+        today = date.today()
+        time_of_buying_tree_years = today.replace(today.year - invest_period)
+        time_of_buying_two_years = today.replace(today.year - invest_period)
+        time_of_buying_one_years = today.replace(today.year - invest_period)
+
+        #down load the stock historical data for recent 3 years
+        yf.pdr_override()
+        stock_history = pdr.get_data_yahoo(tiker, time_of_buying_tree_years, today)
+    
+        # Create a dataframe with only the Adj Close column
+        stock_history_adjclose = stock_history[['Adj Close']]
+        
+        return stock_history_adjclose
+
+    sp500_adjclose_three_years = get_stock_history('^GSPC', 3) #sp500 adjusted closing price in recent 3 years
+    sp500_adjclose_three_years = sp500_adjclose_three_years.rename(columns={'Adj Close':'SP500'})
+    
+    #choosing 5 stocks into the portfolio
+    portfolio_stocks = ["AAPL", "JNJ", "MCD", "MTCH", "NFLX"]
+
+    #down load the historical data for the tocks in the portfolio for recent 3 years
+    portfolio_adjclose_three_years = get_stock_history(portfolio_stocks, 3)
+    portfolio_adjclose_three_years = portfolio_adjclose_three_years['Adj Close']
+    
+    #merge the two data frames
+    merged_portfolio_sp500_three_years = pd.merge(portfolio_adjclose_three_years, sp500_adjclose_three_years, 
+                                                  left_index=True, right_index=True)
+    
+    #plot the portfolio & SP500 history in recent 3 years
+
+    graph_one = []
+    for stock in merged_portfolio_sp500_three_years.columns.tolist():
+          x_val = merged_portfolio_sp500_three_years.index.tolist()
+          y_val = merged_portfolio_sp500_three_years[stock].tolist()
+          graph_one.append(
+              go.Scatter(
+              x = x_val,
+              y = y_val,
+              mode = 'lines',
+              name = stock
+              )
+          )
+    
+    layout_one = dict(title = 'Portfolio and SP500 History in Recent 3 Years',
+                      xaxis = dict(title = 'Time',
+                  ),
+                yaxis = dict(title = 'Price'),
                 )
 
-# second chart plots ararble land for 2015 as a bar chart    
+    #assume the selling date is today and start of the holding date was the three years ago 
+    #swap the rows and columns
+    merged_portfolio_sp500_three_years_start_end = merged_portfolio_sp500_three_years.iloc[[0,-1]]
+    merged_portfolio_sp500_three_years_start_end = merged_portfolio_sp500_three_years_start_end.T
+    merged_portfolio_sp500_three_years_start_end.rename(columns=lambda t: t.strftime('%Y'), inplace=True)
+    merged_portfolio_sp500_three_years_start_end.rename(columns={'2017':'Start', '2020':'End'}, 
+                                                        index = {'Date':'Portfolio'}, inplace=True)
+    merged_portfolio_sp500_three_years_start_end
+    
+    #calculate the three years investment return on the current date
+    merged_portfolio_sp500_three_years_start_end['Return'] = merged_portfolio_sp500_three_years_start_end['End'] / merged_portfolio_sp500_three_years_start_end['Start'] - 1
+    merged_portfolio_sp500_three_years_start_end
+    
+    #average return of the selected portfolio
+    portfolio_return_three_years = merged_portfolio_sp500_three_years_start_end.iloc[0:4]['Return'].mean()
+    
+    #average return of sp500
+    sp500_returen_three_years = merged_portfolio_sp500_three_years_start_end.iloc[-1]['Return']
+    
+    #plot the expected ROI of portfolio vs SP500 in recent 3 years
     graph_two = []
-
     graph_two.append(
-      go.Bar(
-      x = ['a', 'b', 'c', 'd', 'e'],
-      y = [12, 9, 7, 5, 1],
-      )
-    )
+          go.Bar(
+          x = ['SP500', 'Portfolio'],
+          y = [sp500_returen_three_years, portfolio_return_three_years] 
+          )
+        )
 
-    layout_two = dict(title = 'Chart Two',
-                xaxis = dict(title = 'x-axis label',),
-                yaxis = dict(title = 'y-axis label'),
-                )
+    layout_two = dict(title = 'ROI - SP500 VS Portfolio for 3 Years',
+                    xaxis = dict(title = 'SP500 VS Portfolio',),
+                    yaxis = dict(title = 'Return'),
+                    )
+    
+    #prepare data for the 2 years period of investment
+    sp500_adjclose_two_years = get_stock_history('^GSPC', 2) #sp500 adjusted closing price in recent 2 years
+    sp500_adjclose_tow_years = sp500_adjclose_two_years.rename(columns={'Adj Close':'SP500'})
+    portfolio_adjclose_two_years = get_stock_history(portfolio_stocks, 2)
+    portfolio_adjclose_two_years = portfolio_adjclose_two_years['Adj Close']
+    merged_portfolio_sp500_two_years = pd.merge(portfolio_adjclose_two_years, sp500_adjclose_two_years, left_index=True,
+                                                right_index=True)
 
+    #assume the selling date is today and start of the holding date was the two years ago 
+    #swap the rows and columns
+    merged_portfolio_sp500_two_years_start_end = merged_portfolio_sp500_two_years.iloc[[0,-1]]
+    merged_portfolio_sp500_two_years_start_end = merged_portfolio_sp500_two_years_start_end.T
+    merged_portfolio_sp500_two_years_start_end.rename(columns=lambda t: t.strftime('%Y'), inplace=True)
+    merged_portfolio_sp500_two_years_start_end.rename(columns={'2018':'Start', '2020':'End'}, index = {'Date':'Portfolio'},
+                                                      inplace=True)
 
-# third chart plots percent of population that is rural from 1990 to 2015
+    #calculate the two years investment return on the current date
+    merged_portfolio_sp500_two_years_start_end['Return'] = merged_portfolio_sp500_two_years_start_end['End'] /
+    merged_portfolio_sp500_two_years_start_end['Start'] - 1
+
+    #average return of the selected portfolio
+    portfolio_return_two_years = merged_portfolio_sp500_two_years_start_end.iloc[0:4]['Return'].mean()
+    sp500_returen_two_years = merged_portfolio_sp500_two_years_start_end.iloc[-1]['Return']
+    
+    #plot the expected ROI of portfolio vs SP500 in recent 2 years
     graph_three = []
     graph_three.append(
-      go.Scatter(
-      x = [5, 4, 3, 2, 1, 0],
-      y = [0, 2, 4, 6, 8, 10],
-      mode = 'lines'
-      )
-    )
+          go.Bar(
+          x = ['SP500', 'Portfolio'],
+          y = [sp500_returen_two_years, portfolio_return_two_years] 
+          )
+        )
 
-    layout_three = dict(title = 'Chart Three',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label')
-                       )
+    layout_three = dict(title = 'ROI - SP500 VS Portfolio for 2 Years',
+                    xaxis = dict(title = 'SP500 VS Portfolio',),
+                    yaxis = dict(title = 'Return'),
+                    )
     
-# fourth chart shows rural population vs arable land
+    #prepare data for the 1 years period of investment
+    sp500_adjclose_one_years = get_stock_history('^GSPC', 1) #sp500 adjusted closing price in recent 1 years
+    sp500_adjclose_one_years = sp500_adjclose_one_years.rename(columns={'Adj Close':'SP500'})
+    portfolio_adjclose_one_years = get_stock_history(portfolio_stocks, 1)
+    portfolio_adjclose_one_years = portfolio_adjclose_one_years['Adj Close']
+    merged_portfolio_sp500_one_years = pd.merge(portfolio_adjclose_one_years, sp500_adjclose_one_years, left_index=True,
+                                                right_index=True)
+
+    #assume the selling date is today and start of the holding date was the two years ago 
+    #swap the rows and columns
+    merged_portfolio_sp500_one_years_start_end = merged_portfolio_sp500_one_years.iloc[[0,-1]]
+    merged_portfolio_sp500_one_years_start_end = merged_portfolio_sp500_one_years_start_end.T
+    merged_portfolio_sp500_one_years_start_end.rename(columns=lambda t: t.strftime('%Y'), inplace=True)
+    merged_portfolio_sp500_one_years_start_end.rename(columns={'2019':'Start', '2020':'End'}, index = {'Date':'Portfolio'}, inplace=True)
+
+    #calculate the two years investment return on the current date
+    merged_portfolio_sp500_one_years_start_end['Return'] = merged_portfolio_sp500_one_years_start_end['End'] /
+    merged_portfolio_sp500_one_years_start_end['Start'] - 1
+
+    #average return of the selected portfolio
+    portfolio_return_one_years = merged_portfolio_sp500_one_years_start_end.iloc[0:4]['Return'].mean()
+    sp500_returen_one_years = merged_portfolio_sp500_one_years_start_end.iloc[-1]['Return']
+    
+    #plot the expected ROI of portfolio vs SP500 in recent 1 years
     graph_four = []
-    
     graph_four.append(
-      go.Scatter(
-      x = [20, 40, 60, 80],
-      y = [10, 20, 30, 40],
-      mode = 'markers'
-      )
-    )
-
-    layout_four = dict(title = 'Chart Four',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label'),
-                )
+          go.Bar(
+          x = ['SP500', 'Portfolio'],
+          y = [sp500_returen_one_years, portfolio_return_one_years] 
+          )
+        )
     
+    layout_four = dict(title = 'ROI - SP500 VS Portfolio for 1 Years',
+                    xaxis = dict(title = 'SP500 VS Portfolio',),
+                    yaxis = dict(title = 'Return'),
+                    )
+
+
     # append all charts to the figures list
     figures = []
     figures.append(dict(data=graph_one, layout=layout_one))
